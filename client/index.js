ddb498638a1593b4d6168744f50a91f4fe57d776
Augusto Alvarez <augusto.h.alvarez@gmail.com>
2018-01-07 21:30:03 +0100

    Fix serving root component

--- client/index.js
+++ client/index.js
@@ -2,12 +2,12 @@ import React from 'react';
 import { render } from 'react-dom';
 import Button from 'material-ui/Button';
 
-function App() {
-  return (
-    <Button>
-      Hello World
-    </Button>
-  );
-}
 
-render(<App />, document.querySelector('#app'));
+const App = () =>
+  <h1>WHATSAPPPPPP</h1>
+  <Button raised color="primary">
+    Hello World
+  </Button>
+
+
+render(<App />, document.getElementById('app'));
